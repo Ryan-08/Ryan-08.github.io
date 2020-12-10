bdc40fcd246edccf78f2dc63d62b70a3a8967646
Ryan-08 <maskuri.opo8@gmail.com>
2020-12-10 17:10:12 +0700

    edit js

--- js/app.js
+++ js/app.js
@@ -168,20 +168,16 @@ function swicthItem() {
     while (switching) {
       // Start by saying: no switching is done:
       switching = false;
-      b = list.getElementsByTagName("LI");
-      console.log("while");
+      b = list.getElementsByTagName("LI");      
       // Loop through all list items:
       for (i = 0; i < (b.length - 1); i++) {
         // Start by saying there should be no switching:
-        shouldSwitch = false;
-        console.log("for");
+        shouldSwitch = false;        
         /* Check if the next item should
-        switch place with the current item: */
-        console.log(b[i].className);
+        switch place with the current item: */        
         if (b[i].className == 'listed checked' && b[i].className != b[i+1].className) {
           /* If next item is alphabetically lower than current item,
-          mark as a switch and break the loop: */
-          console.log("check");
+          mark as a switch and break the loop: */          
           shouldSwitch = true;
           break;
         }
@@ -190,8 +186,7 @@ function swicthItem() {
         /* If a switch has been marked, make the switch
         and mark the switch as done: */
         b[i].parentNode.insertBefore(b[i + 1], b[i]);
-        // b[i].parentNode.insertBefore(b[i], b[b.length-1]);
-        console.log("swicth");
+        // b[i].parentNode.insertBefore(b[i], b[b.length-1]);        
         switching = true;
       }
     }
